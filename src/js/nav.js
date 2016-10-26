01ee22b94265ea08d5d3255aba94f52b1e6430e8
Phil <philiphussak@Philips-MacBook-Pro.local>
2016-10-27 01:24:23 +0200

    nav.js bugfixes
    nav.js bugfixes

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function($) {
     /*
     ** Mobile navigation slide toggle animation
     */
-    var headerHeight = $('header').outerHeight() + 12;
+    var getHeaderHeight = $('header').outerHeight(),
+        headerPadding = parseInt(getHeaderHeight) + 12,
+        headerHeight = headerPadding.toString() + 'px';
 
     $('.toggle-nav-mobile').click(function () {
         $('nav').css('padding-top', headerHeight);
@@ -12,9 +14,15 @@ jQuery(document).ready(function($) {
 
     $(window).resize(function() {
         if ($(window).width() > 780) {
-            $('nav').css('display', '');
-            $('nav').css('padding-top', '');
-            $('nav').css('height', '100%');
+            if ($('nav').css('display') === 'none') {
+                $('nav').css('display', '');
+            }
+            if ($('nav').css('padding-top') === headerHeight) {
+                $('nav').css('padding-top', '');
+            }
+            if ($('nav').css('height') !== '100%') {
+                $('nav').css('height', '100%');
+            }
         }
     });
 });
