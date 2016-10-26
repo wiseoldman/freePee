740e1de810382a9e3f4c2a48abefcdfc64e5d282
Phil <philiphussak@Philips-MacBook-Pro.local>
2016-10-26 19:53:35 +0200

    Fixes for the navigation on small devices
    Fixes for the navigation on small devices

--- src/js/nav.js
+++ src/js/nav.js
@@ -7,14 +7,18 @@ jQuery(document).ready(function($) {
     $('.toggle-nav-mobile').click(function () {
         $('nav').css('top', headerHeight);
         $('nav').stop().slideToggle();
+        $('body').toggleClass('no-scroll');
         $('.toggle-nav-mobile').toggleClass('is-open');
     });
 
     $(window).resize(function() {
-        if ($(window).width() > 780 && $('nav').css('display') === 'none' || $('nav').css('top', headerHeight)) {
+        if ($(window).width() > 780 && ($('nav').css('display') === 'none' || $('nav').css('top', headerHeight))) {
             $('nav').css('display', '');
             $('nav').css('top', '');
             $('nav').css('height', '100%');
+            if ($('body').hasClass('no-scroll')) {
+                $('body').toggleClass('no-scroll');
+            }
         }
     });
 });
