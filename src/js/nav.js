dbc5c10e59a9fef2c8022404b63673d84bd906e6
Phil <philiphussak@Philips-MacBook-Pro.local>
2016-10-27 00:54:50 +0200

    Navigation fix for iPhones
    Navigation fix for iPhones, a small margin at the top appeared, it’s
    gone now

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,23 +2,22 @@ jQuery(document).ready(function($) {
     /*
     ** Mobile navigation slide toggle animation
     */
-    var headerHeight = $('header').outerHeight();
+    var headerHeight = $('header').outerHeight() + 12;
 
     $('.toggle-nav-mobile').click(function () {
         $('nav').css('padding-top', headerHeight);
-        //$('body').toggleClass('no-scroll');
         $('nav').stop().slideToggle();
         $('.toggle-nav-mobile').toggleClass('is-open');
     });
 
     $(window).resize(function() {
-        if ($(window).width() > 780 && ($('nav').css('display') === 'none' || $('nav').css('top', headerHeight))) {
-            $('nav').css('display', '');
-            $('nav').css('top', '');
-            $('nav').css('height', '100%');
-            /*if ($('body').hasClass('no-scroll')) {
-                $('body').toggleClass('no-scroll');
-            }*/
+        if ($(window).width() > 780) {
+            if ($('nav').css('display', 'none')) {
+                $('nav').css('display', '');
+            } else if ($('nav').css('padding-top', headerHeight)) {
+                $('nav').css('padding-top', '');
+            }
+            $('nav').css('height', '100%');            
         }
     });
 });
