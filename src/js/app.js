8be32ba55366ebd8b14b1cb9e3c388f551e41dd1
Phil <philiphussak@Philips-MacBook-Pro.local>
2016-11-02 12:31:08 +0100

    Using the build in tools to get current position
    Using the build in tools to get current position

--- src/js/app.js
+++ src/js/app.js
@@ -31,17 +31,4 @@
             vm.map.hideInfoWindow('toilet-iw');
         };
     });
-
-    myApp.controller('GetUserPositionController', function($scope) {
-        var options = {
-            enableHighAccuracy: true
-        };
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            $scope.position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-        },
-        function(error) {
-            alert('Unable to get location: ' + error.message);
-        }, options);
-    });
-
 })();
